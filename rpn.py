9f1ef17eea1ef43b2546c1f4fee5fd00cafcd371
axhindol <axhindol@umich.edu>
2017-03-14 16:40:26 -0400

    coveralls

--- rpn.py
+++ rpn.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import operator
+import readline
+import sys
+from termcolor import colored, cprint
 
 operateDict = {
     '+': operator.add,
@@ -11,17 +14,28 @@ operateDict = {
 
 def calculate(arg):
     stack = list()
+    # counter handles more than two inputs
+    counter = 0;
 
     for operand in arg.split():
         try:
+            if (counter == 3):
+                print ("Sorry, you can only input two values")
+                return 0
+            
             operand = float(operand)
             stack.append(operand)
+            counter += 1
 
         except ValueError:
             value2 = stack.pop()
             value1 = stack.pop()
+
+            #print colored output
+            print (colored(value1, 'red') , colored(operand, 'blue') , colored(value2, 'red'), colored('=', 'cyan'), )
             result = operateDict[operand](value1,value2)
             stack.append(result)
+            counter = 0
 
     return stack.pop()
 
@@ -29,7 +43,8 @@ def calculate(arg):
 def main():
     while True:
         res = calculate(input("rpn calc> "))
-        print ("result:", res)
+        if res:
+            print (res)
 
 
 if __name__ == '__main__':  # Note: that's "underscore underscore n a m e ..."
